e005a83adb1609b6bb78ecc1178afa771995438c
Marina <marina_ciocirlan2007@yahoo.com>
2017-06-23 12:54:20 +0300

    added models

--- src/main/java/systems/ab4/workshop/spots/api/MobileAPIController.java
+++ src/main/java/systems/ab4/workshop/spots/api/MobileAPIController.java
@@ -1,15 +1,18 @@
 package systems.ab4.workshop.spots.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import systems.ab4.workshop.spots.domain.spot.Activity;
 import systems.ab4.workshop.spots.domain.spot.Spot;
 import systems.ab4.workshop.spots.domain.spot.SpotRepository;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,13 +27,28 @@ public class MobileAPIController {
 
     @Autowired
     public MobileAPIController(SpotRepository spotRepository){
+
         this.spotRepository = spotRepository;
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public @ResponseBody List<Spot> search
+            (
+        @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") Date startDate,
+        @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") Date endDate,
+        @RequestParam(value = "activities", required = false) List<Activity> activities,
+        @RequestParam(value = "location", required = false) Long location
+            )
+    {
+        return spotRepository.findByActivitiesInAndStartSeasonBeforeAndEndSeasonAfterAndLocationIdOrderByCost(activities,startDate,endDate,location);
+    }
+
+
     public @ResponseBody List<Spot> sayHello(@RequestParam(value="name", required=false) String name) {
         List<Spot> list = new ArrayList<>();
         list.add(spotRepository.findOne(1L));
         return list;
     }
+
+
 }
--- src/main/java/systems/ab4/workshop/spots/domain/spot/SpotRepository.java
+++ src/main/java/systems/ab4/workshop/spots/domain/spot/SpotRepository.java
@@ -1,7 +1,12 @@
 package systems.ab4.workshop.spots.domain.spot;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Alexandru Artimon
@@ -9,4 +14,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource
 public interface SpotRepository extends CrudRepository<Spot, Long> {
+    List<Spot> findByActivitiesInAndStartSeasonBeforeAndEndSeasonAfterAndLocationIdOrderByCost ( @Param("activities") List<Activity> activities, @Param("startSeason") Date startSeason ,@Param("endSeason") Date endSeason ,@Param("location") Long location);
 }
+
+
